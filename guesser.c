b0f353045694ddf7c03449ed28b4d9f2652c1261
LucasAWorkman <lucas.workman@bsu.edu>
2025-01-17 16:21:41 +0000

    .

--- guesser.c
+++ guesser.c
@@ -6,8 +6,15 @@
 //void(tryAgain = 0);
 
 
+typedef enum {
+  false,
+  true
+}
+// i saw this method on stack overflow, it works but im not really sure enum meaning
+bool_enum;
+
 int main(){
-  
+  bool_enum keepGoing = true;
   char userName[20];
   printf ("What is your name? ");
 
@@ -17,6 +24,29 @@ int main(){
   
 
   srand(time(NULL));
-  int r = rand() % 100;
-  printf("The random number is %d \n", r);
+  int randomnum = rand() % 101;
+  int guesses = 0;
+  while (keepGoing == true){
+    guesses++;
+    printf("turn %d ) Please guess a number: ", guesses);
+    int userGuess; 
+    scanf("%d", &userGuess); 
+
+    if (userGuess > randomnum){
+	printf("Too high\n");
+     }
+    if (userGuess < randomnum){
+	printf("Too low\n");
+     }
+    if (userGuess == randomnum){
+	printf("You guessed correct!\n");
+	if (guesses <= 7) {
+	  printf("You got it within seven guesses, good job!\n");
+	}
+	else{
+	  printf("You didnt get it within seven guesses, but thats ok!\n");
+	 }
+	keepGoing = false;
+     }
+  }
 }
